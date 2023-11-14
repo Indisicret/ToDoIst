c94f19dbeba729a3239dfea5b7f5da18689e39de
Indisicret <indisicret@mail.ru>
2023-11-14 12:24:26 +0300

    feat: добавлен компонет регистрации

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { AuthorizationComponent } from './components/authorization/authorization.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 export const routes: Routes = [
     {
         path:'login', component:AuthorizationComponent
+    },
+    {
+        path:'registration', component:RegistrationComponent
     }
 ];
