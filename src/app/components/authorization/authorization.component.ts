5227684c9a6e24ba86971b73775d75eef4ad120b
Indisicret <indisicret@mail.ru>
2023-11-14 20:39:53 +0300

    feat: добавлены компоненты primeng

--- src/app/components/authorization/authorization.component.ts
+++ src/app/components/authorization/authorization.component.ts
@@ -4,9 +4,11 @@ import { RouterLink } from '@angular/router';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
 import { AuthorizationForm } from '../../config/types';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 @Component({
   standalone: true,
-  imports: [CommonModule, RouterLink, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, RouterLink, FormsModule, ReactiveFormsModule, InputTextModule,ButtonModule],
   templateUrl: './authorization.component.html',
   styleUrl: './authorization.component.scss',
 })
