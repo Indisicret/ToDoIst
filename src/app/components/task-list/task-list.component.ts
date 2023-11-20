2cfea7c6319beb888675999d265fe8154db0f70b
Indisicret <indisicret@mail.ru>
2023-11-20 13:55:49 +0300

    feat: добавление таблицы задач

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,16 +1,44 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TableModule }  from 'primeng/table';
+import { TableModule } from 'primeng/table';
+import { Column, Task } from '../../config/types';
+
 
 @Component({
   standalone: true,
-  imports: [
-    CommonModule,
-    TableModule
-  ],
+  imports: [CommonModule, TableModule],
   templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.scss'
+  styleUrl: './task-list.component.scss',
 })
 export class TaskListComponent {
-
+  tasks: Task[] = [
+    {
+      id: 1,
+      name: 'Кофе',
+      description: 'Налить воды в чайник',
+      deadLineDate: '07.11.2033',
+      category: 'Дом',
+    },
+    {
+      id: 2,
+      name: ' Покормить кота',
+      description: 'заказать корм в доставке, поменять воду',
+      deadLineDate: '07.11.2033',
+      category: 'Дом',
+    },
+    {
+      id: 3,
+      name: 'Задание для стажерки',
+      description: 'создать таск менеджер, согласно тз',
+      deadLineDate: '23.12.2023',
+      category: 'Работа',
+    },
+  ];
+  cols: Column[] = [
+    { field: 'id', header: '№' },
+    { field: 'name', header: 'Название' },
+    { field: 'description', header: 'Описание' },
+    { field: 'deadLineDate', header: 'Срок выполнения' },
+    { field: 'category', header: 'Категория' },
+  ];
 }
--- src/app/config/types.ts
+++ src/app/config/types.ts
@@ -1,4 +1,5 @@
 import { FormControl } from '@angular/forms';
+import { type } from 'os';
 
 export type AuthorizationForm = {
   login: FormControl<string | null>;
@@ -17,3 +18,15 @@ export type User = {
   name: string;
   gender: string;
 };
+
+export type Task = {
+  id: number;
+  name: string;
+  category: string;
+  deadLineDate: string;
+  description: string;
+}
+export type Column = {
+  field: string;
+  header: string;
+}
