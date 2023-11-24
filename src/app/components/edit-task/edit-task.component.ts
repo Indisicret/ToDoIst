f9bd1d671fa09ebece08717dc57a58457b18c211
Indisicret <indisicret@mail.ru>
2023-11-24 14:23:56 +0300

    feat: добавлен компонент редактирования задач

---
+++ src/app/components/edit-task/edit-task.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-task',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  template: `<p>edit-task works!</p>`,
+  styleUrl: './edit-task.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class EditTaskComponent { }
