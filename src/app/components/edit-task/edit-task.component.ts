34b306d68fd969548c67f6f0798e1818f781436b
Indisicret <indisicret@mail.ru>
2023-11-27 13:32:55 +0300

    feat: реализовано редактирование задачи

--- src/app/components/edit-task/edit-task.component.ts
+++ src/app/components/edit-task/edit-task.component.ts
@@ -1,13 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, LOCALE_ID } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { TaskService } from '../../services/task.service';
 import { DropdownModule } from 'primeng/dropdown';
 import { InputTextModule } from 'primeng/inputtext';
 import { CalendarModule } from 'primeng/calendar';
-import {AddTaskForm, Task} from '../../config/types';
+import { AddTaskForm, Task } from '../../config/types';
 @Component({
   selector: 'app-edit-task',
   standalone: true,
@@ -19,21 +25,20 @@ import {AddTaskForm, Task} from '../../config/types';
     DropdownModule,
     InputTextModule,
     CalendarModule,
-    
   ],
-   templateUrl: './edit-task.component.html',
-  providers: [TaskService,
+  templateUrl: './edit-task.component.html',
+  providers: [
+    TaskService,
     {
       provide: LOCALE_ID,
-      useValue:'ru'
-    }],
+      useValue: 'ru',
+    },
+  ],
   styleUrl: './edit-task.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-
 })
 export class EditTaskComponent {
-  
-editTaskForm: FormGroup<AddTaskForm>;
+  editTaskForm: FormGroup<AddTaskForm>;
   optionsPriority = [
     { label: 'Низкий', value: 'low' },
     { label: 'Средний', value: 'mid' },
@@ -42,26 +47,44 @@ editTaskForm: FormGroup<AddTaskForm>;
   optionsCategory = [
     { label: 'Дом', value: 'Home' },
     { label: 'Работа', value: 'Work' },
-    
   ];
-  constructor(private taskService: TaskService,private dialogRef: DynamicDialogRef, private config:DynamicDialogConfig) {
-    console.log(this.config)
-    const task:Task = this.config.data.task;
+
+  private task: Task;
+
+  constructor(
+    private taskService: TaskService,
+    private dialogRef: DynamicDialogRef,
+    private config: DynamicDialogConfig
+  ) {
+    this.task = this.config.data.task;
 
     this.editTaskForm = new FormGroup<AddTaskForm>({
-      name: new FormControl<string | null>(task.name, [
+      name: new FormControl<string | null>(this.task.name, [
         Validators.required,
         Validators.maxLength(20),
       ]),
-      category: new FormControl<string | null>(task.category),
-      deadLineDate: new FormControl<Date | string | null>(task.deadLineDate),
+      category: new FormControl<string | null>(this.task.category),
+      deadLineDate: new FormControl<Date | string | null>(
+        this.task.deadLineDate
+      ),
       description: new FormControl<string | null>(
-        task.description,
+        this.task.description,
         Validators.maxLength(100)
       ),
-      priority: new FormControl<string | null>(task.priority, [Validators.required]),
+      priority: new FormControl<string | null>(this.task.priority, [
+        Validators.required,
+      ]),
     });
   }
 
-  saveTask(){}
+  saveEditTask() {
+    const newTask: Task = {
+      ...(this.editTaskForm.getRawValue() as Task),
+      userId: this.task.userId,
+      id: this.task.id,
+    };
+    this.taskService.editTask(newTask);
+    this.dialogRef.close()
+    
+  }
 }
