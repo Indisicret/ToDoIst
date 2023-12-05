ca9e1e5b4c3b03d17f195c67f4d61b7ce91c12d5
Indisicret <indisicret@mail.ru>
2023-12-05 13:50:44 +0300

    feat: добавлен guard списка задач

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,18 @@ import { AuthorizationComponent } from './components/authorization/authorization
 import { RegistrationComponent } from './components/registration/registration.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
 import { CategoriesListComponent } from './components/categories-list/categories-list.component';
+import { AccountsGuard } from './guards/accounts.guard';
 
 export const routes: Routes = [
   {
+    path: '',
+    component : TaskListComponent,
+    canActivate: [AccountsGuard]
+  },
+  {
     path: 'authorization',
     component: AuthorizationComponent,
+    
   },
   {
     path: 'registration',
@@ -15,7 +22,8 @@ export const routes: Routes = [
   },
   {
     path: 'tasks',
-    component : TaskListComponent
+    component : TaskListComponent,
+    canActivate: [AccountsGuard]
   },
   {
     path: 'categories', 
---
+++ src/app/guards/accounts.guard.ts
@@ -0,0 +1,13 @@
+import { inject } from '@angular/core';
+import {
+  CanActivateFn,
+  Router
+} from '@angular/router';
+import { UserService } from '../services/user.service';
+
+export const AccountsGuard: CanActivateFn = (
+) => {
+  return inject(UserService).getUserId()
+    ? true
+    : inject(Router).createUrlTree(['/authorization']);
+};
